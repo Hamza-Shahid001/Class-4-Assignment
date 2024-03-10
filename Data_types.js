29df9259ffb430227598b5ce4dc2b3aa99508d8f
Hamza-Shahid001 <hamzaunique001@gmail.com>
2024-03-10 10:16:56 +0500

    first commit

---
+++ Data_types.js
@@ -0,0 +1,19 @@
+// To write the examples of data types
+var a = "Will You come on my Nikkah."; // String Data Type
+console.log(a); // Display the output
+// Example of String data type
+var b = 677; // Number data Type
+console.log(b); // Display the output
+// Example of Number Data type
+var c = b > 440; // Condition to write boolean data type
+console.log(c); // Display the output
+// Example of boolean data type
+var d;
+console.log(d); // Display the output
+// Example of null data type
+var e;
+console.log(e); // Display the output
+// Example of undefined data type
+var f = 7757908572n; // A big number that can't be use as normal number data type
+console.log(f); // Display the output
+// Example of bigint data type
---
+++ Data_types.ts
@@ -0,0 +1,25 @@
+// To write the examples of data types
+
+let a:string = "Will You come on my Nikkah." // String Data Type
+console.log(a) // Display the output
+// Example of String data type
+
+let b: number= 677 // Number data Type
+console.log(b) // Display the output
+// Example of Number Data type
+
+let c: boolean= b>440 // Condition to write boolean data type
+console.log(c) // Display the output
+// Example of boolean data type
+
+let d: null
+console.log(d) // Display the output
+// Example of null data type
+
+let e:undefined
+console.log(e) // Display the output
+// Example of undefined data type
+
+let f:bigint= 7757908572n // A big number that can't be use as normal number data type
+console.log(f) // Display the output
+// Example of bigint data type
